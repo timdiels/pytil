9e18b4ae5314f3e228966c643065fc378c8d31d8
timdiels <timdiels.m@gmail.com>
2016-02-28 15:28:25 +0100

    Add project tools stubs

---
+++ chicken_turtle/test/mksetup.py
@@ -0,0 +1,5 @@
+'''
+- simple: simple project, nothing funky going on
+- missing_dep: project with a dep that omits a dep, but project fixes it by including the missing dep in its own dependencies
+- sip_dep: project with a dependency that uses sip, e.g. PyQt5
+'''
