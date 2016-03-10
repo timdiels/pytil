8fa1bae7df7d7a23e9a97636028c59c74dce682d
timdiels <timdiels.m@gmail.com>
2016-03-10 17:13:10 +0100

    Apply ct-mkproject

--- chicken_turtle_util/version.py
+++
@@ -1,4 +0,0 @@
-# Versions should comply with PEP440.
-# https://www.python.org/dev/peps/pep-0440/
-# https://python-packaging-user-guide.readthedocs.org/en/latest/distributing/#semantic-versioning-preferred
-__version__ = '1.0.0-dev1'
---
+++ project.py
@@ -0,0 +1,56 @@
+project = dict(
+    name='chicken_turtle_util',
+    description="Python 3 utility library",
+    author='Tim Diels',
+    author_email='timdiels.m@gmail.com',
+    readme_file='README.md',
+    url='https://github.com/timdiels/chicken_turtle_util', # project homepage
+    download_url='https://github.com/timdiels/chicken_turtle_util/releases/v{version}.tar.gz', # Template for url to download source archive from. You can refer to the current version with {version}. You can get one from github or gitlab for example.
+    license='LGPL3',
+ 
+    # What does your project relate to?
+    keywords='development util library',
+    
+    # Package indices to release to using `ct-release`
+    # These names refer to those defined in ~/.pypirc.
+    # For pypi, see http://peterdowns.com/posts/first-time-with-pypi.html
+    # For devpi, see http://doc.devpi.net/latest/userman/devpi_misc.html#using-plain-setup-py-for-uploading
+    index_test = 'pypitest',  # Index to use for testing a release, before releasing to `index_production`. `index_test` can be set to None if you have no test index
+    index_production = 'pypi',
+    
+    # https://pypi.python.org/pypi?%3Aaction=list_classifiers
+    # Note: you must add ancestors of any applicable classifier too
+    classifiers='''
+        Natural Language :: English
+        Intended Audience :: Developers
+        Development Status :: 2 - Pre-Alpha
+        Topic :: Software Development
+        Topic :: Software Development :: Libraries
+        Operating System :: POSIX
+        Operating System :: POSIX :: AIX
+        Operating System :: POSIX :: BSD
+        Operating System :: POSIX :: BSD :: BSD/OS
+        Operating System :: POSIX :: BSD :: FreeBSD
+        Operating System :: POSIX :: BSD :: NetBSD
+        Operating System :: POSIX :: BSD :: OpenBSD
+        Operating System :: POSIX :: GNU Hurd
+        Operating System :: POSIX :: HP-UX
+        Operating System :: POSIX :: IRIX
+        Operating System :: POSIX :: Linux
+        Operating System :: POSIX :: Other
+        Operating System :: POSIX :: SCO
+        Operating System :: POSIX :: SunOS/Solaris
+        Operating System :: Unix
+        License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)
+        Programming Language :: Python
+        Programming Language :: Python :: 3
+        Programming Language :: Python :: 3 :: Only
+        Programming Language :: Python :: 3.2
+        Programming Language :: Python :: 3.3
+        Programming Language :: Python :: 3.4
+        Programming Language :: Python :: 3.5
+        Programming Language :: Python :: Implementation
+        Programming Language :: Python :: Implementation :: CPython
+        Programming Language :: Python :: Implementation :: Stackless
+    ''',
+)
