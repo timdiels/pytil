d762294d168afb6a93e44b0975bcb47215d1623c
timdiels <timdiels.m@gmail.com>
2016-03-17 15:31:41 +0100

    Release v1.1.0

--- setup.py
+++ setup.py
@@ -37,6 +37,7 @@ setup(
                        'Programming Language :: Python :: Implementation :: CPython',
                        'Programming Language :: Python :: Implementation :: Stackless'],
     'description': 'Python 3 utility library',
+    'download_url': 'https://github.com/timdiels/chicken_turtle_util/releases/v1.1.0.tar.gz',
     'extras_require': {'test': ['pytest', 'pytest-xdist', 'pytest-env']},
     'install_requires': [],
     'keywords': 'development util library',
@@ -72,5 +73,5 @@ setup(
     'package_data': {},
     'packages': ['chicken_turtle_util', 'chicken_turtle_util.test'],
     'url': 'https://github.com/timdiels/chicken_turtle_util',
-    'version': '0.0.0'}
+    'version': '1.1.0'}
 )
