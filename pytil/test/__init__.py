1a217acf491d77cbb9bedc76f4ef3bc48af274c2
timdiels <timdiels.m@gmail.com>
2018-01-04 18:46:42 +0100

    Fix pytil.test imports

--- pytil/test/__init__.py
+++ pytil/test/__init__.py
@@ -14,3 +14,6 @@
 #
 # You should have received a copy of the GNU Lesser General Public License
 # along with pytil.  If not, see <http://www.gnu.org/licenses/>.
+
+from ._various import *
+from ._xlsx import *
