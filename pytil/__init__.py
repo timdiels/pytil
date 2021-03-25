65b98194adbd9c21387d71792483888970a08b78
Tim Diels <tim@diels.me>
2021-03-25 19:59:31 +0100

    Bump to v8.0.1.dev

--- pytil/__init__.py
+++ pytil/__init__.py
@@ -15,4 +15,4 @@
 # You should have received a copy of the GNU Lesser General Public License
 # along with pytil.  If not, see <http://www.gnu.org/licenses/>.
 
-__version__ = '8.0.0'
+__version__ = '8.0.1.dev'
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 name = 'pytil'
 setup(
-    version='8.0.0',
+    version='8.0.1.dev',
     name=name,
     # Only include {name}/, not e.g. tests/
     packages=find_packages(include=(name, name + '.*')),
