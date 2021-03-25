0f932a854086d0d32de35db981835c2f93ca3154
Tim Diels <tim@diels.me>
2021-03-25 19:53:59 +0100

    Fix: Bump version in setup.py too

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 name = 'pytil'
 setup(
-    version='8.0.0.dev',
+    version='8.0.0',
     name=name,
     # Only include {name}/, not e.g. tests/
     packages=find_packages(include=(name, name + '.*')),
