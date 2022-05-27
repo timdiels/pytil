4118a7604bd961f9085a2e5a34db6760440a2923
Tim Diels <tim@diels.me>
2022-05-27 10:45:41 +0200

    Fix missing conda-convert

--- pytil/__init__.py
+++ pytil/__init__.py
@@ -15,4 +15,4 @@
 # You should have received a copy of the GNU Lesser General Public License
 # along with pytil.  If not, see <http://www.gnu.org/licenses/>.
 
-__version__ = '8.0.4'
+__version__ = '8.0.5'
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 name = 'pytil'
 setup(
-    version='8.0.4',
+    version='8.0.5',
     name=name,
     # Only include {name}/, not e.g. tests/
     packages=find_packages(include=(name, name + '.*')),
