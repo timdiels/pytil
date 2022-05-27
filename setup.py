5840d908469517b902058a087a022116123b91c2
Tim Diels <tim@diels.me>
2022-05-27 11:09:38 +0200

    Bump to v8.0.6.dev

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 name = 'pytil'
 setup(
-    version='8.0.5',
+    version='8.0.6.dev',
     name=name,
     # Only include {name}/, not e.g. tests/
     packages=find_packages(include=(name, name + '.*')),
